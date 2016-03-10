c48c50f8997f0bda3e2601fa175fbf141fac0598
Emilien Taque <eta@zengularity.com>
2016-03-10 23:24:19 +0100

    Initial commit

---
+++ src/Form/Infix.elm
@@ -0,0 +1,39 @@
+module Form.Infix ((:=), (?=), (|:)) where
+
+{-| Form validation infix operators.
+
+@docs (:=), (?=), (|:)
+-}
+
+import Form.Validate exposing (at, maybe, apply)
+
+
+{-| Infix version of `apply`:
+
+Form.succeed SomeRecord
+|: ("foo" := string)
+|: ("bar" := string)
+
+-}
+(|:) : Validation e (a -> b) -> Validation e a -> Validation e b
+(|:) =
+  apply
+
+
+{-| Infix version of `get`.
+
+    "name" := string
+-}
+(:=) : String -> Validation e a -> Validation e a
+(:=) =
+  get
+infixl 7 :=
+
+
+{-| Access given field, wrapped in a `maybe` (Nothing if error).
+
+    "hobby" ?= string
+-}
+(?=) : String -> Validation e a -> Validation e (Maybe a)
+(?=) s v =
+  maybe (get s v)
